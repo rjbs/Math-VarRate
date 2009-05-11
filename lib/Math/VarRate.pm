b1282c705f0e98468e120b529229615d6f1505e6
Ricardo SIGNES <rjbs@cpan.org>
2009-05-10 23:28:36 -0400

    check both ways

--- lib/Math/VarRate.pm
+++ lib/Math/VarRate.pm
@@ -3,8 +3,80 @@ use warnings;
 package Math::VarRate;
 
 sub new {
-  my ($class) = @_;
-  return bless {}  => $class;
+  my ($class, $arg) = @_;
+
+  my $self = bless { rate_changes => $arg->{rate_changes} }  => $class;
+
+  $self->_precompute_offsets;
+
+  return $self;
+}
+
+sub starting_value { 0 }
+
+sub offset_for {
+  my ($self, $value) = @_;
+  $value += 0;
+
+  my $ko       = $self->{known_offsets};
+  my ($offset) = sort { $b <=> $a } grep { $ko->{ $_ } < $value } keys %$ko;
+
+  my $rate     = $self->{rate_changes}{ $offset };
+  my $to_go    = $value - $ko->{ $offset };
+  my $dur      = $to_go / $rate;
+
+  return $offset + $dur;
+}
+
+sub value_at {
+  my ($self, $offset) = @_;
+  $offset += 0;
+
+  my $known_offsets = $self->{known_offsets};
+
+  return $self->__initial_compute_value_at($offset) if ! $known_offsets;
+  return $known_offsets->{ $offset } if exists $known_offsets->{ $offset };
+
+  my ($max) = sort { $b <=> $a } grep { $_ < $offset } keys %$known_offsets;
+
+  my $start = $known_offsets->{ $max };
+  my $rate  = $self->{rate_changes}{ $max };
+  my $dur   = $offset - $max;
+
+  return $start  +  $rate * $dur;
+}
+
+sub __initial_compute_value_at {
+  my ($self, $offset) = @_;
+
+  my $value   = $self->starting_value;
+  my %changes = %{ $self->{rate_changes} || {} };
+
+  my @points = sort { $a <=> $b } grep { $_ < $offset } keys %changes;
+
+  for my $i (0 .. $#points) {
+    my $rate     = $changes{ $points[ $i ] };
+    my $duration = $i == $#points
+                 ? ($offset - $points[ $i ])
+                 : ($points[ $i + 1 ] - $points[ $i ]);
+
+    $value += $rate * $duration;
+  }
+
+  return $value;
+}
+
+sub _precompute_offsets {
+  my ($self) = @_;
+
+  my $value   = {};
+  my %changes = %{ $self->{rate_changes} || {} };
+  
+  for my $offset (keys %changes) {
+    $value->{ $offset } = $self->value_at( $offset );
+  }
+
+  $self->{known_offsets} = $value;
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -11,23 +11,46 @@ use Test::More 'no_plan';
 use Math::VarRate;
 
 my %meter = (
-  ref   => Math::VarRate->new({ rate_changes => [ 0 => 1 ] }),
+  ref   => Math::VarRate->new({ rate_changes => { 0 => 1 } }),
   alpha => Math::VarRate->new({
-    rate_changes => [
+    rate_changes => {
       0   => 1,
       100 => 2.0,
       200 => 1.5,
-    ],
+    },
   }),
   beta  => Math::VarRate->new({
-    rate_changes => [
+    rate_changes => {
       0   => 1,
       100 => 0.5,
       175 => 1,
-    ],
+    },
   }),
 );
 
 isa_ok($meter{$_}, 'Math::VarRate') for keys %meter;
 
-is($meter{$_}->value_at(0), 0, "start $_ meter at 0") for keys %meter;
+is($meter{$_}->value_at(0),  0,  "$_ meter: 0 at 0") for keys %meter;
+is($meter{$_}->value_at(50), 50, "$_ meter: 50 at 50") for keys %meter;
+
+is(
+  $meter{ref}->value_at(300),
+  300,
+  "ref meter: 300 at 300",
+);
+
+is(
+  $meter{alpha}->value_at(300),
+  450,
+  "alpha meter: 450 at 300",
+);
+
+is(
+  $meter{beta}->value_at(300),
+  262.5,
+  "beta meter: 262.5 at 300",
+);
+
+is($meter{ref}->offset_for(300),    300, "ref meter: 300 at 300 (value_at)");
+is($meter{alpha}->offset_for(450),  300, "ref meter: 450 at 300 (value_at)");
+is($meter{beta}->offset_for(262.5), 300, "ref meter: 262.5 at 300 (value_at)");
