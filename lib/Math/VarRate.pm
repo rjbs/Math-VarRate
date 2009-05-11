2dfd65ad912d07e90f74626c15812df899abaa81
Ricardo SIGNES <rjbs@cpan.org>
2009-05-11 10:27:02 -0400

    precompute more efficiently

--- lib/Math/VarRate.pm
+++ lib/Math/VarRate.pm
@@ -108,7 +108,6 @@ sub value_at {
 
   my $known_offsets = $self->{known_offsets};
 
-  return $self->__initial_compute_value_at($offset) if ! $known_offsets;
   return $known_offsets->{ $offset } if exists $known_offsets->{ $offset };
 
   my ($max) = sort { $b <=> $a } grep { $_ < $offset } keys %$known_offsets;
@@ -122,37 +121,28 @@ sub value_at {
   return $start  +  $rate * $dur;
 }
 
-sub __initial_compute_value_at {
-  my ($self, $offset) = @_;
-
-  my $value   = $self->starting_value;
-  my %changes = %{ $self->{rate_changes} };
-
-  my @points = sort { $a <=> $b } grep { $_ < $offset } keys %changes;
-
-  for my $i (0 .. $#points) {
-    my $rate     = $changes{ $points[ $i ] };
-    my $duration = $i == $#points
-                 ? ($offset - $points[ $i ])
-                 : ($points[ $i + 1 ] - $points[ $i ]);
-
-    $value += $rate * $duration;
-  }
-
-  return $value;
-}
-
 sub _precompute_offsets {
   my ($self) = @_;
 
-  my $value   = {};
+  my $value   = $self->starting_value;
+  my $v_at_o  = {};
   my %changes = %{ $self->{rate_changes} };
   
-  for my $offset (keys %changes) {
-    $value->{ $offset } = $self->value_at( $offset );
+  my @points = sort { $a <=> $b } keys %changes;
+  my $prev   = 0;
+  my $rate   = 0;
+
+  for my $offset (sort { $a <=> $b } keys %changes) {
+    my $duration = $offset - $prev;
+
+    $value += $duration * $rate;
+    $v_at_o->{ $offset } = $value;
+
+    $rate = $changes{ $offset };
+    $prev = $offset;
   }
 
-  $self->{known_offsets} = $value;
+  $self->{known_offsets} = $v_at_o;
 }
 
 1;
